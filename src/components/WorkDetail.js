09ff1d14a1a4b647173d0a21e40c047ea9ae42f7
Brian Harvey <brian.harv3y@gmail.com>
2020-12-09 16:15:30 -0800

    build work detail component
    Co-authored-by: grant eadie <grantleadie@gmail.com>
    Co-Authored-by: Alex Goodwin <hagoodwi@gmail.com>

--- src/components/WorkDetail.js
+++ src/components/WorkDetail.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
+function WorkDetail(props) {
+  const { work, onClickingDelete } = props;
 
-function WorkDetail() {
   return (
     <React.Fragment>
       <h3>Work detail </h3>
+      <p>{work.name}</p>
+      <button onClick= { props.onClickingEdit }>Update this Work</button>
+      <button onClick= {() => onClickingDelete(work.id) }>Delete this Work</button>
+      <hr/>
     </React.Fragment>
   )
 }
+
+WorkDetail.propTypes = {
+  work: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+};
 
 export default WorkDetail;
